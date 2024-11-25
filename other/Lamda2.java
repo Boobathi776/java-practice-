b45e57ed6fb06401243bd59b1487a100ac432872
Boobathi776 <boobathir72@gmail.com>
2024-11-25 12:05:45 +0530

    lamda expression with parameter

---
+++ other/Lamda2.java
@@ -0,0 +1,12 @@
+interface A
+{
+  public void not(int a,int b);
+}
+class Lamda2
+{
+public static void main(String[] args) {
+    int a=5,b=5;
+    A obj=(c,d)->{ System.out.println(c+d);};   // lamda expression with more than one parameter
+    obj.not(a,b);
+}
+}
